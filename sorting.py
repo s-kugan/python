f989b17b1d8393e63797b7d0c4b5c411dc4fafa3
s-kugan <sivakugan@gmail.com>
2024-02-02 17:45:34 -0500

    program to sort numbers in the list

--- sorting.py
+++ sorting.py
@@ -1,12 +1,19 @@
+# Define the list of numbers
 dupnums = [1,5,2,9,4,3,100,6,56,89,12,5000]
 
-def sortnum(arg):
-    for i in range(len(arg) - 1, 0, -1):
-        for j in range(i + 1, len(arg)):
-            if arg[i] < arg[j]:
-                temp = arg[i]
-                arg[i] = arg[j]
-                arg[j] = temp
-    return arg
+# Define the selection sort function
+def selection_sort(list):
+  # Loop through the entire list
+  for i in range(len(list)):
+    # Find the index of the smallest element in the unsorted part of the list
+    min_index = i
+    for j in range(i + 1, len(list)):
+      if list[j] < list[min_index]:
+        min_index = j
+    # Swap the smallest element with the first element in the unsorted part of the list
+    list[i], list[min_index] = list[min_index], list[i]
+  # Return the sorted list
+  return list
 
-print(sortnum(dupnums))
+# Call the selection sort function and print the result
+print(selection_sort(dupnums))
